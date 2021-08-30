a62d62c2a371ab383606a5afce9067f6417fdb26
deviprasad11 <63278041+deviprasad11@users.noreply.github.com>
2021-08-30 17:38:18 +0530

    Add files via upload

---
+++ trafic/trafic.js
@@ -0,0 +1,24 @@
+function yellow(){
+setTimeout(function(){ document.getElementById("yelw").style.background= "yellow" 
+
+},5000);	
+
+}
+yellow();
+function red(){
+    setTimeout(function () {
+        document.getElementById("new").style.background = "red",
+        style.background = ""    
+    },10000);	
+    
+    }
+    red();
+    yellow();
+function green(){
+    setTimeout(function(){ document.getElementById("grn").style.background= "green"  
+    },13000);	
+    
+    }
+    green();
+    // setTimeout(yellow(),15000);
+//  }
